4a7ceb108361f67c8a74340c9cdb9802ab99d397
YareniCruz <yarenicruz.050516@gmail.com>
2026-03-09 20:51:40 -0600

    Happy face

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ function draw() {
 }*/
 
 // TRIANGULO
+/*
 function draw() {
   const canvas = document.getElementById("canvas");
   if (canvas.getContext) {
@@ -41,8 +42,24 @@ function draw() {
     ctx.lineTo(100, 25);
     ctx.fill();
   }
-}
+}*/
 
+function draw() {
+  const canvas = document.getElementById("canvas");
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Círculo externo
+    ctx.moveTo(110, 75);
+    ctx.arc(75, 75, 35, 0, Math.PI, false); // Boca (en el sentido de las agujas del reloj)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Ojo izquierdo
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Ojo derecho
+    ctx.stroke();
+  }
+}
 
 
 // Eventos
